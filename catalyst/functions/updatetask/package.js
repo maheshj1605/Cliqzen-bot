4c6db5108084651d75b6902ecc2a2051344e7333
Mahesh-J <mahesh162005k@gmail.com>
2025-11-26 13:27:22 +0530

    Add package.json for updateTask function

--- catalyst/functions/updatetask/package.js
+++ catalyst/functions/updatetask/package.js
@@ -1,1 +1,9 @@
-
+{
+  "name": "updateTask",
+  "version": "1.0.0",
+  "description": "Update Task Function for CliqZen Bot (Catalyst ZCF)",
+  "main": "index.js",
+  "dependencies": {
+    "zcatalyst-sdk-node": "^1.0.0"
+  }
+}
